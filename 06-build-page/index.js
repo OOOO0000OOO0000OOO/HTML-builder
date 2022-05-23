47d93f4acb212f714477d15b1574573ec06eed90
k0ntinuum <st088265@student.spbu.ru>
2022-05-24 02:26:18 +0300

    refactor: refactor 06-build-page task solution

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -2,39 +2,51 @@ const fs = require('fs/promises');
 const path = require('path');
 
 async function copyAssets(src, dest) {
-  const stats = await fs.stat(src);
-  if (stats.isDirectory()) {
-    await fs.mkdir(dest, { recursive: true });
-    const items = await fs.readdir(src, { withFileTypes: true });
-    for (const item of items) {
-      copyAssets(path.join(src, item.name), path.join(dest, item.name));
+  try {
+    const stats = await fs.stat(src);
+    if (stats.isDirectory()) {
+      await fs.mkdir(dest, { recursive: true });
+      const items = await fs.readdir(src, { withFileTypes: true });
+      for (const item of items) {
+        copyAssets(path.join(src, item.name), path.join(dest, item.name));
+      }
+    } else {
+      await fs.copyFile(src, dest);
     }
-  } else {
-    await fs.copyFile(src, dest);
+  } catch (err) {
+    throw `Failed to copy assets.\n${err.message}`;
   }
 }
 
 async function buildHTML() {
-  const components = await fs.readdir(path.join(__dirname, 'components'));
-  let template = await fs.readFile(path.join(__dirname, 'template.html'), 'utf-8');
-  for (const component of components) {
-    const stats = await fs.stat(path.join(__dirname, 'components', component));
-    if (stats.isFile() && path.parse(component).ext === '.html') {
-      const name = path.parse(component).name;
-      const data = await fs.readFile(path.join(__dirname, 'components', component), 'utf-8');
-      template = template.replace(new RegExp(`{{${name}}}`, 'g'), data);
+  try {
+    const components = await fs.readdir(path.join(__dirname, 'components'));
+    let template = await fs.readFile(path.join(__dirname, 'template.html'), 'utf-8');
+    for (const component of components) {
+      const stats = await fs.stat(path.join(__dirname, 'components', component));
+      if (stats.isFile() && path.parse(component).ext === '.html') {
+        const name = path.parse(component).name;
+        const data = await fs.readFile(path.join(__dirname, 'components', component), 'utf-8');
+        template = template.replace(new RegExp(`{{${name}}}`, 'g'), data);
+      }
     }
+    fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), template);
+  } catch (err) {
+    throw `Failed to build HTML.\n${err.message}`;
   }
-  fs.writeFile(path.join(__dirname, 'project-dist', 'index.html'), template);
 }
 
 async function mergeStyles() {
-  const files = await fs.readdir(path.join(__dirname, 'styles'));
-  for (const file of files) {
-    if (path.parse(file).ext === '.css') {
-      const data = await fs.readFile(path.join(__dirname, 'styles', file), 'utf-8');
-      await fs.appendFile(path.join(__dirname, 'project-dist', 'style.css'), data);
+  try {  
+    const files = await fs.readdir(path.join(__dirname, 'styles'));
+    for (const file of files) {
+      if (path.parse(file).ext === '.css') {
+        const data = await fs.readFile(path.join(__dirname, 'styles', file), 'utf-8');
+        await fs.appendFile(path.join(__dirname, 'project-dist', 'style.css'), data);
+      }
     }
+  } catch (err) {
+    throw `Failed to merge styles.\n${err.message}`;
   }
 }
 
@@ -47,7 +59,7 @@ async function buildPage() {
     await mergeStyles();
     return 'The document has been successfully created.';
   } catch (err) {
-    return `Unexpected error!\n${err.message}`;
+    return `Unexpected error!\n${err.message || err}`;
   }
 }
 
